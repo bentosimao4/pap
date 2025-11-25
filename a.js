6d1d99cd84c43bf96da9cd19fcfe8af8bde4e354
bentosimao4 <bentosimao4@gmail.com>
2025-11-25 11:25:46 +0000

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,1 @@
-<button type="button" onclick="document.getElementById('teste').innerHTML = Date()">
-Data</button>
+<button type="button" onclick="document.getElementById('teste').innerHTML = Date()"> Data </button>
